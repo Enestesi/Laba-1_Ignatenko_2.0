ba575bc5de727a3e54256d8e893b55bfb181b8a2
Enestesi <99076321+Enestesi@users.noreply.github.com>
2022-02-11 14:31:18 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -11,8 +11,11 @@ echo "<br>Your order is:$very_bad_unclear_name.";
 //2
 $num1 = 1;
 echo $num1 . "<br>";
-$var2 = 2;
+$num2 = 2;
 echo $num2;
 $num3 = 3.123;
 echo " " . $num3;
 echo 3+9;
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month;
